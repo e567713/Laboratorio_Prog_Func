b06a3d5ee765e3395c110bb3021985ce497d4a45
Ernesto <cora.ernesto@gmail.com>
2018-06-03 19:41:15 -0300

    ahora si

---
+++ lab2018/TypeChecker.hs
@@ -0,0 +1,29 @@
+-- LABORATORIO DE PROGRAMACION FUNCIONAL 2018
+-- MODULO DE CHEQUEO DE NOMBRES Y TIPOS
+
+-- Se debe implementar la funcion checkProgram, que
+-- realiza los chequeos de nombres y tipos como se
+-- especifica en la letra
+
+module TypeChecker where
+
+import Syntax
+-- se pueden agregar mas importaciones 
+-- en caso de ser necesario
+
+
+-- TYPE CHECKER
+
+data Error = Duplicated      Name
+           | Undefined       Name
+           | Expected        Type Type
+
+instance Show Error where
+ show (Duplicated      n)  = "Duplicated definition: " ++ n
+ show (Undefined       n)  = "Undefined: " ++ n
+ show (Expected    ty ty') = "Expected: " ++ show ty ++ " Actual: " ++ show ty'
+
+
+
+checkProgram :: Program -> [Error]
+checkProgram = undefined -- Implementar
