7181fab1e132154a4445c1efc25be75c41bacef2
Ernesto <cora.ernesto@gmail.com>
2018-06-14 21:33:26 -0300

    checker franco

--- lab2018/TypeChecker.hs
+++ lab2018/TypeChecker.hs
@@ -25,5 +25,98 @@ instance Show Error where
 
 
 
+
 checkProgram :: Program -> [Error]
-checkProgram = undefined -- Implementar
+checkProgram (Program defs body) = etapa1 ++ etapa2 ++ etapa3
+where
+	etapa1 = checkNombresRepetidos defs
+	etapa2 = checkNombresNoDeclarados (Program defs body)
+	etapa3 = checkTipos(Program defs body)
+
+checkNombresNoDeclarados :: Program -> [Error]
+checkNombresNoDeclarados (Program defs body) = 
+	usadosIncluidosEnDeclarados (nombresDeclarados defs, nombresUsados body)
+
+usadosIncluidosEnDeclarados :: [Name] -> [Name] -> [Error]
+--Se toma la lista xs como la lista de declarados
+--Se toma la lista ys como la lista de usados
+usadosIncluidosEnDeclarados xs [] = []
+usadosIncluidosEnDeclarados xs ys =
+	[Undefined y | y <- ys, notElem y xs]
+
+nombresUsados :: Body -> [Name]
+nombresUsados [] = []
+nombresUsados (x:xs) = (getBodyName x) : nombresUsados xs
+
+nombresDeclarados :: Defs -> [Name]
+nombresDeclarados [] = []
+nombresDeclarados (x:xs) = (getDefName x) ++ nombresDeclarados xs
+
+getBodyName :: Stmt -> [Name]
+getBodyName (Assig name expr) = [name] ++ getExprName expr  --no se si esta bien
+															--no se que es Assig
+getBodyName (If expr body1 body2) = getExprName expr ++ getBodyName body1 ++ getBodyName body2
+getBodyName (While expr body) = getExprName expr ++ getBodyName body
+getBodyName (Write expr) = getExprName expr
+getBodyName (Read  name) = [name]
+
+getExprName :: Expr -> [Name]
+getExprName (Var name) = [name]
+getExprName (IntLit int) = []
+getExprName (BoolLit bool) = []
+getExprName (Unary uOp expr) = getExprName expr
+getExprName (Binary bOp expr1 expr2) = getExprName expr1 ++ getExprName expr2
+
+
+getDefName :: VarDef -> [Name]
+getDefName (VarDef (name, _)) = [name]
+
+
+checkNombresRepetidos :: Defs -> [Error]
+checkNombresRepetidos (defs) = 
+	checkElementosRepetidos (nombresDeclarados defs)
+
+checkElementosRepetidos :: [Name] -> [Error]
+checkElementosRepetidos [] = []
+checkElementosRepetidos [x] = []
+checkElementosRepetidos (x:xs)
+	| elem (last xs) (x:(init xs)) = (checkElementosRepetidos (x:(init xs))) ++ [Duplicated (last xs)]
+	| otherwise = checkElementosRepetidos (x:(init xs))
+
+--*****************************************
+--De aca en adelante se hace el check de tipos
+--*****************************************
+
+checkTipos :: Program -> [Error]
+checkTipos (Program defs []) = []
+--*******************************
+checkTipos (Program defs (x:xs)) = checkStmt x ++ checkTipos (Program defs xs)
+
+
+--Chequea instrucciones if, while, readln, writeln
+--checkStmt :: Stmt -> [Error]
+
+--*******************************
+
+--Devuelve lista de variables (Names) que deben ser booleanas
+--Que son usadas en if, while
+listaNamesQueDeberianSerTipoBool :: Stmt -> [Error]
+--Assig Name Expr .... no se que es
+listaNamesQueDeberianSerTipoBool (If expr body1 body2) = 
+	esTipoBool expr ++
+	listaNamesQueDeberianSerTipoBool body1 ++
+	listaNamesQueDeberianSerTipoBool body2
+listaNamesQueDeberianSerTipoBool (While expr body) =
+	esTipoBool expr ++
+	listaNamesQueDeberianSerTipoBool body 
+listaNamesQueDeberianSerTipoBool (Write expr) = 
+
+
+	getNameTipoBool expr ++ 
+	listaNamesQueDeberianSerTipoBool body1 ++
+	listaNamesQueDeberianSerTipoBool body2
+listaNamesQueDeberianSerTipoBool (While expr body)
+
+tipoCorrecto :: Body -> [Error]
+tipoCorrecto [] = []
+tipoCorrecto (x:xs) = 
