c9a2d0857465ee67457d988d551f2e240f121cc6
Franco Wanseele <frandi95@hotmail.com>
2018-06-16 14:41:08 -0300

    optimizer

--- lab2018/Optimizer.hs
+++ lab2018/Optimizer.hs
@@ -16,4 +16,22 @@ import Syntax
 
 
 optimize :: Program -> Program
-optimize = undefined -- Implementar 
+optimize (Program name defs []) = Program name defs body
+optimize (Program name defs (x:xs)) = Program name defs optimizeStmt x : optimizeBody xs
+
+
+optimizeBody :: Body -> Body
+optimizeBody name defs [] = Program name defs body 
+optimizeBody name defs (x:xs) = Program name defs optimize x ++ optimizeBody xs
+
+optimizeStmt :: Stmt -> Body 
+optimizeStmt (Assig name expr) = [(Assig name optimizeExpr expr)]
+optimizeStmt (If expr body1 body2) 
+	| alwaysTrue expr = optimizeBody body1
+	| alwaysFalse expr = optimizeBody body2
+	| otherwise = [(If optimizeExpr expr optimizeBody body1 optimizeBody body2)]
+optimizeStmt (While expr body)
+	| alwaysFalse = []
+	| otherwise = [(While optimizeExpr expr optimizeBody body)]
+optimizeStmt (Write expr) = [(Write optimizeExpr expr)]
+optimizeStmt (Read  name) = [(Read name)]
