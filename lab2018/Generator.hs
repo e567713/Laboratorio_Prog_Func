ccffc95749bf50f2b2326a12fe43436582bfc381
Ernesto <cora.ernesto@gmail.com>
2018-06-17 14:27:27 -0300

    generator 2

--- lab2018/Generator.hs
+++ lab2018/Generator.hs
@@ -26,7 +26,11 @@ generateStmt :: Stmt -> Code
 generateStmt (Assig name expr) = generateExpr expr ++ [STORE name]
 generateStmt (Read name) = [READ] ++ [STORE name]
 generateStmt (Write expr) = generateExpr(expr) ++ [WRITE]
-generateStmt (If expr body1 body2) = generateExpr(expr) ++ []
+generateStmt (If expr body1 body2) = generateExpr(expr)  ++ [JMPZ 3] ++ [PUSH 0] ++ [JUMP 2] ++ [PUSH 1] ++
+           [SKIP] ++ [JMPZ (length(generateBody(body1))+2)] ++ generateBody(body1) ++ [JUMP (length(generateBody(body2))+1)]++
+           generateBody(body2) ++ [SKIP]
+generateStmt(While expr body1) = generateExpr(expr) ++ [JMPZ 3] ++ [PUSH 0] ++ [JUMP 2] ++ [PUSH 1] ++ [SKIP] ++ [JMPZ (length(generateBody(body1))+2)] ++
+        generateBody(body1) ++ [JUMP (-(length(generateBody(body1))+length(generateExpr(expr))+6))] ++ [SKIP]
 generateStmt stmt = []
 
 
@@ -34,9 +38,14 @@ generateExpr :: Expr -> Code
 generateExpr (IntLit int) = [PUSH int]
 generateExpr (BoolLit bool) = if bool then [PUSH 1] else [PUSH 0]
 generateExpr (Var name) = [LOAD name]
+generateExpr (Unary uOp expr1)
+  | uOp == Neg = generateExpr (expr1) ++ [NEG]
 generateExpr (Binary bOp expr1 expr2)
   | bOp == Plus = generateExpr (expr2) ++ generateExpr(expr1) ++ [ADD]
   | bOp == Equ = generateExpr (expr2) ++ generateExpr(expr1) ++ [CMP]
+  | bOp == Less = generateExpr (expr2) ++ generateExpr(expr1) ++ [CMP] ++ [PUSH 1] ++ [ADD]
+  | bOp == Minus = generateExpr (expr2) ++ generateExpr(expr1) ++ [SUB]
+  | bOp == Mult = generateExpr (expr2) ++ generateExpr(expr1) ++ [MUL]
 generateExpr _ = []
 
 
